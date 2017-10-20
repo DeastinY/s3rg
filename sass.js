07c02e93e033908a55d65396d8f5a4906252d3fe
DeastinY <polzin.richard@gmail.com>
2017-10-20 10:45:06 +0200

    updated with grid

--- sass.js
+++ sass.js
@@ -63,7 +63,7 @@ module.exports = {
        */
       excludeFiles: [
         /\.(wp|ios).(scss)$/i,
-        /(action-sheet|alert|backdrop|badge|checkbox|chip|datetime|fab|grid|icon|img|infinite-scroll|input|item|label|list|loading|menu|modal|note|picker|popover|radio|range|refresher|searchbar|segment|select|show-hide-when|slides|split-pane|spinner|tabs|toast|toggle|virtual-scroll|cordova)/i,
+        /(action-sheet|alert|backdrop|badge|checkbox|chip|datetime|fab|icon|img|infinite-scroll|input|item|label|list|loading|menu|modal|note|picker|popover|radio|range|refresher|searchbar|segment|select|show-hide-when|slides|split-pane|spinner|tabs|toast|toggle|virtual-scroll|cordova)/i,
       ],
     
       /**
